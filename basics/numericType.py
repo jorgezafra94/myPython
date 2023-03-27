7a96775784631f4d5e5473b20309d5a508a48b2c
jorge <jorgezafra94@hotmail.com>
2023-03-26 21:57:57 -0500

    numeric types

---
+++ basics/numericType.py
@@ -0,0 +1,20 @@
+# there are 4 types of numeric variables
+intType = 98
+print(f"{intType} - {type(intType)}")
+
+floatType = 19.98
+print(f"{floatType} - {type(floatType)}")
+
+complexType = 1 + 2j
+print(f"{complexType} - {type(complexType)}")
+
+# Arithmetic operations
+x = 1
+x += x  # x = x + 1
+x -= x  # x = x - 1
+x *= 2  # x =  x * 2
+x /= 2  # x = x / 2
+x //= 2  # x = x // 2
+x **= 4  # x = x ** 4
+x %= 2  # x = x % 2
+
